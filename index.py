5bc42e729a1b10711d2b176868908ec4ac0016c3
Bayangmbe Mounmo <bayangp0@gmail.com>
2022-06-20 12:03:00 +0100

    Adde route for grabbing data

--- index.py
+++ index.py
@@ -1,9 +1,42 @@
-from flask import Flask
+#!/usr/bin/env python
+# coding: utf8
+
+from flask import Flask, jsonify
+import pandas as pd
 
 
 app = Flask(__name__)
 
 
+def load_forecast():
+    """
+    Read the forecast
+    """
+    return pd.read_pickle("forecast.pkl")
+
 @app.route('/')
 def home():
-    return {"response": "THE API ENDPOINT format is /town/dataset/datetime "}
+    """
+    Show the defaulkt format of the principal ENDPOINT
+    """
+    return {"response": "THE API ENDPOINT format is /<town>/<dataset>/<datetime> "}
+
+
+@app.route('/<town>/<dataset>/<datetime>') #<>
+def _data(town, dataset, datetime):
+    """
+    grab data the forecast model
+    """
+
+    forecast = load_forecast()
+    d = forecast
+    row_date = d[ d["date"] == datetime+" 00:00:00" ]
+    row_town = row_date[row_date["town"] == town]
+    row_dataset = row_town[row_town["dataset"] == dataset]
+    value = row_dataset["ValueF"].values
+    print(value)
+
+    return {
+        "response": 'The data from the <'+dataset+'> dataset, at <'+town+'> on <'+datetime+'>',
+        "value": value[0]
+    }
