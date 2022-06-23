06e2b0ff664c2915c46fa343bd4ba1360a4c0b53
Bayangmbe Mounmo <bayangp0@gmail.com>
2022-06-23 09:06:01 +0100

    Reduced all line of main.py to max 75 colulns

--- main.py
+++ main.py
@@ -11,8 +11,9 @@ CURRENT_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
 if __name__ == "__main__":
     app = QGuiApplication(sys.argv)
     engine = QQmlApplicationEngine()
+    path_font = "Comfortaa/Comfortaa-Regular.ttf"
 
-    font = os.path.join(CURRENT_DIRECTORY, "font", "Comfortaa/Comfortaa-Regular.ttf")
+    font = os.path.join(CURRENT_DIRECTORY, "font", path_font)
     _id = QFontDatabase.addApplicationFont(font)
 
     engine.load(os.path.join(os.path.dirname(__file__), "qml/main.qml"))
