065800b910ae222d4086e5487f1c613907ccad08
Bayangmbe Mounmo <bayangp0@gmail.com>
2022-06-25 04:15:00 +0100

    added

--- Worker.py
+++ Worker.py
@@ -1,6 +1,17 @@
 # This Python file uses the following encoding: utf-8
 from PySide2.QtCore import QObject, Signal, Slot, Property
 
-class Worker:
+class Worker(QObject):
+
+
     def __init__(self):
-        pass
+        QObject.__init__(self)
+        self.str = "Class for Slots"
+
+
+    @Slot(str, str, result="QVariantList")
+    def wdata(self):
+        """
+        weather data pulled from vacator.vercel.app/
+        """
+        return [37, 25.4, 0.005]
