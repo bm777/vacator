df622ea9d6d4b9e2c855c2ff11c4c85cef3d3c7d
Bayangmbe Mounmo <bayangp0@gmail.com>
2022-06-25 04:15:19 +0100

    added

--- Worker.py
+++ Worker.py
@@ -12,6 +12,6 @@ class Worker(QObject):
     @Slot(str, str, result="QVariantList")
     def wdata(self):
         """
-        weather data pulled from vacator.vercel.app/
+        weather data pulled from vacator.vercel.app/ 
         """
         return [37, 25.4, 0.005]
